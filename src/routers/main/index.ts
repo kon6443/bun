14d3260d487c60c33a72f50ff3c55c4c3784a674
Onam <kon6443@gmail.com>
2025-11-29 17:37:37 +0900

    UPDATE: Added sign in & up loginType

--- src/routers/main/index.ts
+++ src/routers/main/index.ts
@@ -3,11 +3,13 @@ import express from "express";
 import mainRouter from "./mainRouter";
 import authRouter from "./authRouter";
 import discordRouter from "./discordRouter";
+import teamRouter from "./teamRouter";
 
 const router = express.Router();
 
 router.use("/", mainRouter);
 router.use("/auth", authRouter);
 router.use("/discord", discordRouter);
+router.use("/teams", teamRouter);
 
 export default router;
