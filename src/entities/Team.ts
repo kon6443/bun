185ebef586534e7390836411e9963effda2e640c
Onam Kwon <kon6443@gmail.com>
2026-01-12 15:49:05 +0900

    Added teamDescription field

--- src/entities/Team.ts
+++ src/entities/Team.ts
@@ -1,32 +1,42 @@
-import "reflect-metadata";
-import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, OneToMany, JoinColumn, ManyToMany } from "typeorm";
-import { User } from "./User";
-import { TeamMember } from "./TeamMember";
+import 'reflect-metadata';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  OneToMany,
+  JoinColumn,
+  ManyToMany,
+} from 'typeorm';
+import { User } from './User';
+import { TeamMember } from './TeamMember';
 
-@Entity("TEAMS")
+@Entity('TEAMS')
 export class Team {
-  @PrimaryGeneratedColumn({ name: "TEAM_ID" })
+  @PrimaryGeneratedColumn({ name: 'TEAM_ID' })
   teamId: number;
 
-  @Column({ name: "TEAM_NAME", type: "varchar2" })
+  @Column({ name: 'TEAM_NAME', type: 'varchar2' })
   teamName: string;
 
-  @Column({ name: "LEADER_ID", type: "number" })
+  @Column({ name: 'TEAM_DESCRIPTION', type: 'varchar2' })
+  teamDescription: string | null;
+
+  @Column({ name: 'LEADER_ID', type: 'number' })
   leaderId: number;
 
-  @Column({ name: "CRTD_AT", type: "timestamp" })
+  @Column({ name: 'CRTD_AT', type: 'timestamp' })
   crtdAt: Date;
-  
-  @Column({ name: "ACT_STATUS", type: "number" })
+
+  @Column({ name: 'ACT_STATUS', type: 'number' })
   actStatus: number;
 
   // 관계
-  @ManyToMany(() => User, (user) => user.userId)
-  @JoinColumn({ name: "LEADER_ID" })
+  @ManyToMany(() => User, user => user.userId)
+  @JoinColumn({ name: 'LEADER_ID' })
   users: User[];
 
   // @OneToMany(() => TeamMember, (teamMember) => teamMember.teamId)
-  @ManyToMany(() => TeamMember, (teamMember) => teamMember.teamId)
+  @ManyToMany(() => TeamMember, teamMember => teamMember.teamId)
   teamMembers: TeamMember[];
 }
-
--- src/modules/team/dto/create-team.dto.ts
+++ src/modules/team/dto/create-team.dto.ts
@@ -1,12 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsNotEmpty } from 'class-validator';
+import { IsString, IsNotEmpty, MaxLength, IsOptional } from 'class-validator';
 
 export class CreateTeamDto {
   @ApiProperty({ description: '팀 이름', example: '개발팀' })
   @IsString()
   @IsNotEmpty()
   teamName: string;
+
+  @ApiProperty({ description: '팀 설명', example: '팀 설명은 최대 100자 까지 입력 가능합니다.' })
+  @IsString()
+  @MaxLength(100)
+  @IsOptional()
+  teamDescription: string;
+
   actStatus: number;
+
   leaderId: number;
 }
-
