32d6508c780690e40fd070dc8a822ccdf9c7546a
Onam <kon6443@gmail.com>
2026-01-24 14:13:50 +0900

    telegram 챗봇 채팅방 연동 기능 추가

--- src/modules/notification/notification.module.ts
+++ src/modules/notification/notification.module.ts
@@ -1,14 +1,16 @@
 import { Module, Global } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DiscordController } from './discord.controller';
+import { TelegramController } from './telegram.controller';
 import { DiscordService } from './discord.service';
 import { TelegramService } from './telegram.service';
 import { Team } from '../../entities/Team';
+import { TelegramLink } from '../../entities/TelegramLink';
 
 @Global()
 @Module({
-  imports: [TypeOrmModule.forFeature([Team])],
-  controllers: [DiscordController],
+  imports: [TypeOrmModule.forFeature([Team, TelegramLink])],
+  controllers: [DiscordController, TelegramController],
   providers: [DiscordService, TelegramService],
   exports: [DiscordService, TelegramService],
 })
