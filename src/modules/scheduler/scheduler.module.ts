4b3ba2a1a3762a51e2d9c05e29d869277248989d
Onam Kwon <kon6443@gmail.com>
2026-03-31 17:37:17 +0900

    태스크 활성상태 기능 수정 및 스케쥴러 추가

--- src/modules/scheduler/scheduler.module.ts
+++ src/modules/scheduler/scheduler.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { SchedulerService } from './scheduler.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../../entities/User';
+import { TeamTask } from '../../entities/TeamTask';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, TeamTask])],
   providers: [SchedulerService],
 })
 export class SchedulerModule {}
