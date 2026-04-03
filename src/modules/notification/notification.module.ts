a9394290f8be2a11c7648b6708bbb165495d7628
Onam <kon6443@gmail.com>
2026-04-04 00:48:41 +0900

    Applied Port/Adapter pattern

--- src/modules/notification/notification.module.ts
+++ src/modules/notification/notification.module.ts
@@ -3,7 +3,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TelegramController } from './telegram.controller';
 import { DiscordService } from './discord.service';
 import { TelegramService } from './telegram.service';
-import { NotificationService } from './notification.service';
+import { NotificationAdapter } from './notification.adapter';
+import { NOTIFICATION_PORT } from '../../common/port/notification.port';
 import { Team } from '../../entities/Team';
 import { TelegramLink } from '../../entities/TelegramLink';
 
@@ -11,7 +12,12 @@ import { TelegramLink } from '../../entities/TelegramLink';
 @Module({
   imports: [TypeOrmModule.forFeature([Team, TelegramLink])],
   controllers: [TelegramController],
-  providers: [DiscordService, TelegramService, NotificationService],
-  exports: [DiscordService, TelegramService, NotificationService],
+  providers: [
+    DiscordService,
+    TelegramService,
+    NotificationAdapter,
+    { provide: NOTIFICATION_PORT, useExisting: NotificationAdapter },
+  ],
+  exports: [DiscordService, TelegramService, NOTIFICATION_PORT],
 })
 export class NotificationModule {}
